0692dd7b8a8f33ea56647bea792ec23b1610e83e
Antoine JEAN <jenaraw91@gmail.com>
2022-11-09 14:50:31 +0100

    initialisation des tests

---
+++ test/test_main.py
@@ -0,0 +1,19 @@
+def test_default():
+    assert 0 == 0
+
+
+
+
+
+
+
+# -------------- À ajouter si problème avec l'executable ----------------
+# def resource_path(relative_path):
+#     """ Get absolute path to resource, works for dev and for PyInstaller """
+#     try:
+#         # PyInstaller creates a temp folder and stores path in _MEIPASS
+#         base_path = sys._MEIPASS
+#     except Exception:
+#         base_path = os.path.abspath(".")
+#
+#     return os.path.join(base_path, relative_path)
