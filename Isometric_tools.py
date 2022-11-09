f6eae247257fd3e893d22bd856c344a182df1b33
TomBessonBerthaut <bessonberthaut@et.esiea.fr>
2022-11-09 13:50:36 +0100

    Avec des commentaires

--- Isometric_tools.py
+++ Isometric_tools.py
@@ -6,7 +6,7 @@ class Isometric_tools:
         self.origine_y = height_window - self.height_block
 
     def coordinate_to_pixel(self, x_coord, y_coord):
-        x_pixel = 0.5 * self.width_block * (x_coord - y_coord) + self.origine_x  # x_pixel = l'origine + la moitié de la lageur du block * x_coord - la moitié de la lageur du block * y_coord
+        x_pixel = 0.5 * self.width_block * (x_coord - y_coord) + self.origine_x  # x_pixel = l'origine + la moitié de la largeur du block * x_coord - la moitié de la largeur du block * y_coord
         y_pixel = -0.25 * self.height_block * (x_coord + y_coord) + self.origine_y  # y_pixel = l'origine - le quarte de la hauteur du block * x_coord - le quarte de la hauteur du block * y_coord
 
         return x_pixel, y_pixel
@@ -18,7 +18,7 @@ class Isometric_tools:
         return x_coord, y_coord
 
     def coordinate_to_pixel_z(self, x_coord, y_coord, z_coord):
-        x_pixel = 0.5 * self.width_block * (x_coord - y_coord) + self.origine_x  # x_pixel = l'origine + la moitié de la lageur du block * x_coord - la moitié de la lageur du block * y_coord
-        y_pixel = self.height_block * (0.5 * z_coord - 0.25 * (x_coord + y_coord)) + self.origine_y  # y_pixel = l'origine + la moitier de la hauteur du block * z_coord - le quarte de la hauteur du block * x_coord - le quarte de la hauteur du block * y_coord
+        x_pixel = 0.5 * self.width_block * (x_coord - y_coord) + self.origine_x  # x_pixel = l'origine + la moitié de la largeur du block * x_coord - la moitié de la largeur du block * y_coord
+        y_pixel = self.height_block * (0.5 * z_coord - 0.25 * (x_coord + y_coord)) + self.origine_y  # y_pixel = l'origine + la moitié de la hauteur du block * z_coord - le quarte de la hauteur du block * x_coord - le quarte de la hauteur du block * y_coord
 
         return x_pixel, y_pixel
