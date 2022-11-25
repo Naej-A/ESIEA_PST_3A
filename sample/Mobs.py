8b4e24f59fba25aba11984ccc3403183b8c21b55
TomBessonBerthaut <bessonberthaut@et.esiea.fr>
2022-11-25 13:59:52 +0100

    Mobs work in progress

---
+++ sample/Mobs.py
@@ -0,0 +1,12 @@
+class Mobs:
+
+    def __init__(self, blockX, blockY, name):
+
+        self.blockX = blockX
+        self.blockY = blockY
+        self.pv
+        self.speed
+
+    def move(self):
+
+        self.blockX
